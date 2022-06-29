5464d73c6ab5f163630290b29e5ef5350aa7827c
ElmiraBehnava <elmira.behnava@yahoo.com>
2022-06-29 18:19:55 +0430

    feat(back): setup settings

--- back/settings/common.py
+++ back/settings/common.py
@@ -1,11 +1,12 @@
 """Common settings for django project."""
 
-from pathlib import Path
-from os.path import abspath, dirname, join
-from sys import path
 from os import environ
+from os.path import abspath, dirname, join
+from pathlib import Path
+from sys import path
 
-## GET ENV UTIL
+
+# GET ENV UTIL
 def get_env(key, default=None, optinal=False):
     """Return environment variables with some options."""
     val = environ.get(key)
@@ -15,66 +16,61 @@ def get_env(key, default=None, optinal=False):
         return default
     elif not optinal:
         raise ValueError(f"Environment variable {key} was not defined")
-## END GET ENV UTIL
+# END GET ENV UTIL
 
 
-## PATH CONFIGURATION
+# PATH CONFIGURATION
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_ROOT = dirname(dirname(abspath(__file__)))
 PACKAGE_ROOT = dirname(PROJECT_ROOT)
 # Add our project to our pythonpath, this way we don't need to type our project
 # name in our dotted import paths:
 path.append(PACKAGE_ROOT)
-## END PATH CONFIGURATION
+# END PATH CONFIGURATION
 
-## SITE CONFIGURATION
+# SITE CONFIGURATION
 SITE_NAME = get_env("SITE_NAME")
 SITE_ID = int(get_env("SITE_ID"))
-## SITE CONFIGURATION
+# SITE CONFIGURATION
 
-## DEBUG CONFIGURATION
+# DEBUG CONFIGURATION
 DEBUG = get_env("DEBUG", default="False") == "True"
 TEMPLATE_DEBUG = DEBUG
-## END DEBUG CONFIGURATION
+# END DEBUG CONFIGURATION
 
-## APP CONFIGURATION
+# APP CONFIGURATION
 DJANGO_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # Admin panel and documentation:
     "django.contrib.admin",
-    'django.contrib.admindocs',
+    "django.contrib.admindocs",
 )
 
-THIRD_PARTY_APPS = ()
+THIRD_PARTY_APPS = ("rest_framework",)
 
 # Apps specific for this project go here.
 LOCAL_APPS = ("src.api",)
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
-## END APP CONFIGURATION
+# END APP CONFIGURATION
 
-## MIDDLEWARE CONFIGURATION
+# MIDDLEWARE CONFIGURATION
 MIDDLEWARE = (
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 )
-## END MIDDLEWARE CONFIGURATION
+# END MIDDLEWARE CONFIGURATION
 
-## URL CONFIGURATION
-ROOT_URLCONF = 'src.urls'
-## END URL CONFIGURATION
-
-## TEMPLATE CONFIGURATION
+# TEMPLATE CONFIGURATION
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -90,73 +86,86 @@ TEMPLATES = [
         },
     },
 ]
-## END TEMPLATE CONFIGURATION
+# END TEMPLATE CONFIGURATION
 
-## WSGI CONFIGURATION
-WSGI_APPLICATION = 'core.wsgi.application'
-## END WSGI CONFIGURATION
+# WSGI CONFIGURATION
+WSGI_APPLICATION = "core.wsgi.application"
+# END WSGI CONFIGURATION
 
-## DATABASE CONFIGURATION
+# DATABASE CONFIGURATION
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": get_env("DEFAULT_DATABASE_NAME"),
+        "USER": get_env("DEFAULT_DATABASE_USER"),
+        "PASSWORD": get_env("DEFAULT_DATABASE_PASSWORD"),
+        "HOST": get_env("DEFAULT_DATABASE_HOST"),
+        "PORT": get_env("DEFAULT_DATABASE_PORT"),
     }
 }
-## END DATABASE CONFIGURATION
+# END DATABASE CONFIGURATION
 
-## Password validation CONFIGURATION
+# Password validation CONFIGURATION
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation"
+            ".UserAttributeSimilarityValidator"
+        ),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation.MinimumLengthValidator"
+        ),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation.CommonPasswordValidator"
+        ),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation.NumericPasswordValidator"
+        ),
     },
 ]
-## END Password validation CONFIGURATION
+# END Password validation CONFIGURATION
 
-## GENERAL CONFIGURATION
-LANGUAGE_CODE = get_env("LANGUAGE_CODE", default='en-us')
+# GENERAL CONFIGURATION
+LANGUAGE_CODE = get_env("LANGUAGE_CODE", default="en-us")
 
-TIME_ZONE = get_env("TIME_ZONE", default='UTC')
+TIME_ZONE = get_env("TIME_ZONE", default="UTC")
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-## END GENERAL CONFIGURATION
+# END GENERAL CONFIGURATION
 
-## MEDIA CONFIGURATION
+# MEDIA CONFIGURATION
 MEDIA_ROOT = get_env("MEDIA_ROOT", default="media/")
 MEDIA_URL = get_env("MEDIA_URL", default="media/")
-## END MEDIA CONFIGURATION
+# END MEDIA CONFIGURATION
 
-## STATIC FILE CONFIGURATION
+# STATIC FILE CONFIGURATION
 STATIC_ROOT = get_env("STATIC_ROOT", default="static/")
 STATIC_URL = get_env("STATIC_URL", default="static/")
-## END STATIC FILE CONFIGURATION
+# END STATIC FILE CONFIGURATION
 
-## SECRET CONFIGURATION
+# SECRET CONFIGURATION
 SECRET_KEY = get_env("SECRET_KEY")
-## END SECRET CONFIGURATION
+# END SECRET CONFIGURATION
 
-## SITE CONFIGURATION
+# SITE CONFIGURATION
 ALLOWED_HOSTS = get_env("ALLOWED_HOSTS", default="*").split(",")
-## END SITE CONFIGURATION
+# END SITE CONFIGURATION
 
-## FIXTURE CONFIGURATION
+# FIXTURE CONFIGURATION
 FIXTURE_DIRS = (Path(join(PROJECT_ROOT, "fixtures")),)
-## END FIXTURE CONFIGURATION
+# END FIXTURE CONFIGURATION
 
-## LOGGING CONFIGURATION
+# LOGGING CONFIGURATION
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
@@ -177,8 +186,63 @@ LOGGING = {
         },
     },
 }
-## END LOGGING CONFIGURATION
+# END LOGGING CONFIGURATION
 
-## DEFAULT_AUTO_FIELD CONFIGURATION
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-## END DEFAULT_AUTO_FIELD CONFIGURATION
+# DEFAULT_AUTO_FIELD CONFIGURATION
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# END DEFAULT_AUTO_FIELD CONFIGURATION
+
+# RESTFARMEWORK CONFIGURATION
+REST_FRAMEWORK = {
+    "DEFAULT_THROTTLE_RATES": {
+        "otp_hour": get_env("OTP_HOUR"),
+        "otp_day": get_env("OTP_DAY"),
+    },
+    "EXCEPTION_HANDLER": "src.api.exception_handler.api_exception_handler",
+    # "DEFAULT_AUTHENTICATION_CLASSES": (
+    #     "src.apps.authentication.backends.JWTAuthentication",
+    # ),
+}
+# END RESTFARMEWORK CONFIGURATION
+
+# CACHE CONFIGURATION
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": get_env("REDIS_URL"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
+# END CACHE CONFIGURATION
+
+# JWT CONFIGURATION
+ACCESS_TTL = int(get_env("ACCESS_TTL"))
+REFRESH_TTL = int(get_env("REFRESH_TTL"))
+JWT_SECRET = get_env("JWT_SECRET")
+if (
+    len(JWT_SECRET.encode("utf-8")) < 64
+    or len(JWT_SECRET.encode("utf-8")) > 128
+):
+    raise ValueError("JWT_SECRET must be between 64 and 128 bytes")
+# END JWT CONFIGURATION
+
+# DEVELOPMENT MODE CONFIGURATION
+UNDER_DEVELOPMENT = get_env("UNDER_DEVELOPMENT", default="False") == "True"
+# END DEVELOPMENT MODE CONFIGURATION
+
+
+# AUTH_USER_MODEL CONFIGURATION
+# AUTH_USER_MODEL = "accounts_new.User"
+# END AUTH_USER_MODEL CONFIGURATION
+
+# NATS CONFIGURATION
+NATS_CLUSTER_ID = get_env("NATS_CLUSTER_ID")
+NATS_URL = get_env("NATS_URL")
+# END NATS CONFIGURATION
+
+# URL CONFIGURATION
+ROOT_URLCONF = "src.urls"
+# END URL CONFIGURATION
+
